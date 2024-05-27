2b743bfe877a48e1db7e6f3407656cd24f1c8a6c
Ctato1 <dznelashvilitato@gmail.com>
2024-05-27 19:30:20 +0400

    add kanban task navigation

--- src/app/kanban/name/name.component.ts
+++ src/app/kanban/name/name.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Route} from "@angular/router";
-import {BoardsService} from "../../shared/boards.service";
+import {ActivatedRoute, Params, Route} from "@angular/router";
+import {BoardsService, BoardsServiceProps} from "../../shared/boards.service";
 
 @Component({
   selector: 'app-name',
@@ -8,13 +8,23 @@ import {BoardsService} from "../../shared/boards.service";
   styleUrl: './name.component.css'
 })
 export class NameComponent implements OnInit{
-
+  currentRoute!:string;
+  currentBoard?:BoardsServiceProps;
   constructor(private boardsService: BoardsService,private route:ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.route.params.subscribe(res=> {
+    this.route.params.subscribe((res:Params)=> {
       this.boardsService.route.next(res['name'])
+      this.currentRoute = res['name']
+      this.currentBoard = this.boardsService.getBoardByName(this.currentRoute);
+      this.boardsService.currentBoard.subscribe((item:BoardsServiceProps)=>{
+        this.currentBoard = item;
+        console.log(this.currentBoard)
+      })
+
+
     })
+
   }
 }
--- src/app/shared/boards.service.ts
+++ src/app/shared/boards.service.ts
@@ -1,34 +1,108 @@
 import {Injectable} from "@angular/core";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, Subject} from "rxjs";
 
-interface BoardsServiceProps{
+interface items{
+  type:string;
+  description?:string;
+  subtasks?:{checked:boolean;name:string}[]
+}
+export interface BoardsServiceProps{
   name:string,
-  items:string[],
+  items:items[],
 }
 @Injectable({providedIn:"root"})
 
 export class BoardsService {
   showBoard = new BehaviorSubject<boolean>(true);
   route = new BehaviorSubject<string>("");
+  currentBoard:any = new Subject<BoardsServiceProps[]>();
 
   boards:BoardsServiceProps[] = [
     {
       name:"Platform Launch",
-      items:["Build Platform for onboarding flow"]
+      items:[
+        {
+          type:"Todo",
+          description:"We know what we're planning to build for version one." +
+            " Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.",
+          subtasks:[
+            {
+              checked:true,
+              name:"Research competitor pricing and business models",
+            },
+            {
+              checked:false,
+              name:"Outline a business model that works for our solution",
+            },
+            {
+              checked:false,
+              name:"Talk to potential customers about our proposed solution and ask for fair price expectancy",
+            }
+          ]
+
+        },
+      ]
     },
     {
       name:"Marketing Plan",
-      items:["Build Marketing for onboarding flow"]
+      items:[
+        {
+          type:"Doing",
+          description:"We know what we're planning to build for version one." +
+            " Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.",
+          subtasks:[
+            {
+              checked:true,
+              name:"Research competitor pricing and business models",
+            },
+            {
+              checked:false,
+              name:"Outline a business model that works for our solution",
+            },
+            {
+              checked:false,
+              name:"Talk to potential customers about our proposed solution and ask for fair price expectancy",
+            }
+          ]
+
+        },
+      ]
     },
     {
       name:"Roadmap",
-      items:["Build Roadmap for onboarding flow"]
-    }
+      items:[
+        {
+          type:"Done",
+          description:"We know what we're planning to build for version one." +
+            " Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.",
+          subtasks:[
+            {
+              checked:true,
+              name:"Research competitor pricing and business models",
+            },
+            {
+              checked:false,
+              name:"Outline a business model that works for our solution",
+            },
+            {
+              checked:false,
+              name:"Talk to potential customers about our proposed solution and ask for fair price expectancy",
+            }
+          ]
+
+        },
+      ]
+    },
   ];
 
   createBoard(name:string){
     this.boards.push({name:name,items:[]})
     return this.boards.slice();
+  }
+  getBoardByName(name:string){
+    let currentBoard:BoardsServiceProps = this.boards.filter(item=>item.name === name).slice()[0];
+    this.currentBoard.next(currentBoard)
+    return currentBoard;
   }
 
   getAllName(){
